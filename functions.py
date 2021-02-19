53ab22bee613ec4b0641b729931a27f4e26d62e0
Jose C. Garcia Alanis <12409129+JoseAlanis@users.noreply.github.com>
2021-02-19 17:04:37 +0100

    fix wx issues

--- functions.py
+++ functions.py
@@ -7,43 +7,54 @@ def randomlist():
     psych = [7, 8, 9, 10, 11, 12]
     return bio, psych
 
-# 2) Initialize the gui windows, they well be updated later
-# define gui
-myDlg = gui.Dlg(title="SFB_289")
-myDlgbio = gui.Dlg(title="Quiz")
-myDlgpsych = gui.Dlg(title="Quiz")
-
 
 # 3) Adapt gui widows accordingly
 def sub_id():
+    myDlg = gui.Dlg(title="SFB_289")
     myDlg.addField('Subject ID:')
-    myDlg.addField('mzp:', )
-    ok_data = myDlg.show()  # show dialog and wait for OK or Cancel
-    if myDlg.OK:  # or if ok_data is not None
+    myDlg.addField('mzp:')
+
+    # show dialog box and wait for subject input,
+    # then OK or Cancel
+    ok_data = myDlg.show()
+
+    # if OK pressed show data, otherwise cancel
+    if myDlg.OK:
         print(ok_data)
     else:
         print('user cancelled')
+
+    # clean up
+    del myDlg
+
     return ok_data
 
 
 def quiz_bio():
-    myDlgbio.addField('Frage 1:', choices =["A1", "A2", "A3", "A4"])
-    myDlgbio.addField('Frage 2:', choices =["A1", "A2", "A3", "A4"])
+    myDlgbio = gui.Dlg(title="Quiz")
+    myDlgbio.addField('Frage 1:', choices=["A1", "A2", "A3", "A4"])
+    myDlgbio.addField('Frage 2:', choices=["A1", "A2", "A3", "A4"])
     myDlgbio.addField('Frage 3:', choices=["A1", "A2", "A3", "A4"])
     myDlgbio.addField('Frage 4:', choices=["A1", "A2", "A3", "A4"])
 
-    # show dialog and wait for OK or Cancel
+    # show dialog box and wait for subject input,
+    # then OK or Cancel
     ok_databio = myDlgbio.show()
 
-    # or if ok_data is not None
+    # if OK pressed show data, otherwise cancel
     if myDlgbio.OK:
         print(ok_databio)
     else:
         print('user cancelled')
+
+    # clean up
+    del myDlgbio
+
     return ok_databio
 
 
 def quiz_psych():
+    myDlgpsych = gui.Dlg(title="Quiz")
     myDlgpsych.addField('Frage 1:', choices=["A1", "A2", "A3", "A4"])
     myDlgpsych.addField('Frage 2:', choices=["A1", "A2", "A3", "A4"])
     myDlgpsych.addField('Frage 3:', choices=["A1", "A2", "A3", "A4"])
@@ -57,4 +68,8 @@ def quiz_psych():
         print(ok_datapsych)
     else:
         print('user cancelled')
+
+    # clean up
+    del myDlgpsych
+
     return ok_datapsych
